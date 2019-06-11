5c456643320c2363d783d3b3a78cf423eb3cd653
Anssi Herranen <anssi.herranen@vincit.fi>
2019-06-11 14:50:48 +0300

    Initial webmanifest bundling

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
+import '../css/global.css'
 import classes from '../css/main.css'
 
 export default () => {
-  console.log(classes.main)
+
+  const { main } = classes
+  const contentNode = document.getElementById('content')
+
+  const newElement = document.createElement('div')
+  newElement.classList.add(main)
+  newElement.innerText = 'XD'
+
+  contentNode.appendChild(newElement)
+
 }
